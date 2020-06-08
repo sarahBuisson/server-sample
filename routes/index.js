eaea430d4af8d0b09c1b60c716e6fe3ca4eedbc6
sarah buisson <sarahbuisson@gmail.com>
2020-06-08 18:39:43 +0200

    change router

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/info', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
