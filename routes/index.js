c01a2413c87e67b8beefb74f191ff8df6abb2fdb
sarah buisson <sarahbuisson@gmail.com>
2020-06-10 19:40:03 +0200

    save scores

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-
+let scores=[]
 router.get('/info', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/score', function(req, res, next) {
+  scores.push(req.body)
+  if(scores.length>10)
+    scores=scores.slice(1)
+  res.render('Ok' );
+});
+
+router.get('/scores', function(req, res, next) {
+  res.render(scores);
+});
+
+
+
 
 module.exports = router;
