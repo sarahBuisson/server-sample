9aabf086cb6c4f18029e82ff6709fd251843ce7f
sarah buisson <sarahbuisson@gmail.com>
2020-06-10 20:14:25 +0200

    formatting

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-let scores=[]
-router.get('/info', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let scores = []
+router.get('/info', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.post('/score', function(req, res, next) {
-  scores.push(req.body)
-  if(scores.length>10)
-    scores=scores.slice(1)
-  res.render('Ok' );
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/scores', function(req, res, next) {
-  res.render(scores);
+router.post('/score', function (req, res, next) {
+    scores.push(req.body)
+    if (scores.length > 10)
+        scores = scores.slice(1)
+    res.send('Ok');
 });
 
-
+router.get('/scores', function (req, res, next) {
+    res.send(scores);
+});
 
 
 module.exports = router;
