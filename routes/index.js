d04170c18ef44d3e9645fa40cea898233fe17cf1
sarah buisson <sarahbuisson@gmail.com>
2020-06-11 10:43:32 +0200

    manage cors

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors')
+
+
+var whitelist = ['http://localhost:4200', 'http://sarahbuisson.github.io']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 /* GET home page. */
 let scores = []
@@ -11,14 +24,14 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
-router.post('/score', function (req, res, next) {
+router.post('/score',  cors(corsOptionsDelegate),function (req, res, next) {
     scores.push(req.body)
     if (scores.length > 10)
         scores = scores.slice(1)
     res.status(200).send('Ok');
 });
 
-router.get('/scores', function (req, res, next) {
+router.get('/scores',  cors(corsOptionsDelegate),function (req, res, next) {
     res.status(200).send(scores);
 });
 
