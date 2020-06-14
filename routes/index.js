38834deec0466ef65315c94bba27d6ae7c3e193c
sarah buisson <sarahbuisson@gmail.com>
2020-06-14 16:11:20 +0200

    log origin

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,11 @@ var cors = require('cors')
 var whitelist = ['http://localhost:4200', 'http://sarahbuisson.github.io']
 var corsOptionsDelegate = function (req, callback) {
     var corsOptions;
+    console.log(req.header('Origin'))
     if (whitelist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+        corsOptions = {origin: true} // reflect (enable) the requested origin in the CORS response
     } else {
-        corsOptions = { origin: false } // disable CORS for this request
+        corsOptions = {origin: false} // disable CORS for this request
     }
     callback(null, corsOptions) // callback expects two parameters: error and options
 }
@@ -24,14 +25,14 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
-router.post('/score',  cors(corsOptionsDelegate),function (req, res, next) {
+router.post('/score', cors(corsOptionsDelegate), function (req, res, next) {
     scores.push(req.body)
     if (scores.length > 10)
         scores = scores.slice(1)
     res.status(200).send('Ok');
 });
 
-router.get('/scores',  cors(corsOptionsDelegate),function (req, res, next) {
+router.get('/scores', cors(corsOptionsDelegate), function (req, res, next) {
     res.status(200).send(scores);
 });
 
@@ -40,7 +41,7 @@ router.get('/scoresText', function (req, res, next) {
 });
 
 router.get('/scoresJson', function (req, res, next) {
-    res.status(200).json({scores:scores});
+    res.status(200).json({scores: scores});
 });
 
 
